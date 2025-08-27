2a9344811bd3eb9b5b217462d33427f1aba44bd1
larsatsas <97964877+larsatsas@users.noreply.github.com>
2025-08-27 13:19:55 +0200

    Update bootstrap_singlestore.ps1
    Fixed the newline issue

--- bootstrap_singlestore.ps1
+++ bootstrap_singlestore.ps1
@@ -5,6 +5,8 @@
 # * kubeconfig for your environment downloaded and stored at .kube/engagekube
 # * Run from your home folder
 #
+Powershell kode
+# Set environment variables
 $env:KUBECONFIG = ".kube/engagekube"
 $viya_namespace = "viya"
 $pod = "node-sas-singlestore-cluster-master-0"
@@ -14,6 +16,7 @@ $secret = kubectl -n $viya_namespace get secret sas-singlestore-cluster -o json
 $passwordEncoded = ($secret | ConvertFrom-Json).data.ROOT_PASSWORD
 $password = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($passwordEncoded))
 
+# now with a fix on the new line...
 # Create post_config.sql
 @"
 SET GLOBAL table_name_case_sensitivity = OFF;
@@ -22,13 +25,14 @@ CREATE USER IF NOT EXISTS 'sas' IDENTIFIED BY 'Orion123';
 CREATE DATABASE IF NOT EXISTS myDB;
 GRANT SHOW METADATA ON *.* to 'sas';
 GRANT ALL ON myDB.* TO 'sas';
-"@ | Set-Content -Path "post_config.sql"
+"@ | Set-Content -Path "post_config.sql" -NoNewline
 
 # Create setupS2.sh
 @"
 export MYSQL_PWD=$password
 singlestore < /tmp/post_config.sql
-"@ | Set-Content -Path "setupS2.sh"
+"@ | Set-Content -Path "setupS2.sh" -NoNewline
+
 
 # Copy files to the pod
 kubectl -n $viya_namespace cp post_config.sql "${pod}:/tmp"
@@ -36,3 +40,4 @@ kubectl -n $viya_namespace cp setupS2.sh "${pod}:/tmp"
 
 # Execute the setup script inside the pod
 kubectl -n $viya_namespace exec $pod -- /bin/bash /tmp/setupS2.sh
+
