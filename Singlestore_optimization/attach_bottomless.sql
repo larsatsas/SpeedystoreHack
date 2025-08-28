fc36d3427af6d70564f84d7bf492ca911c3cffcb
larsatsas <97964877+larsatsas@users.noreply.github.com>
2025-08-28 11:03:50 +0200

    Typos fixed

--- Singlestore_optimization/attach_bottomless.sql
+++ Singlestore_optimization/attach_bottomless.sql
@@ -1,3 +1,3 @@
 ATTACH DATABASE myDB_BL ON AZURE '<containername>'
       CONFIG '{}'  
-      CREDENTIALS '{"account_name":"<your storage account name at Azure>","account_key":"<access key to this store account>"}';
+      CREDENTIALS '{"account_name":"<your storage account name at Azure>","account_key":"<access key to this storage account>"}';
--- Singlestore_optimization/bottomless_singlestore.sql
+++ Singlestore_optimization/bottomless_singlestore.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE myDB_BL ON AZURE '<containername>'
       CONFIG '{}'  
-      CREDENTIALS '{"account_name":"<your storage account name at Azure>","account_key":"<access key to this store account>"}';
+      CREDENTIALS '{"account_name":"<your storage account name at Azure>","account_key":"<access key to this storage account>"}';
